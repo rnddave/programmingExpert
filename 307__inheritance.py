87600e0553b0bf01b454751dd894898ecf89e90b
David Dickinson <rnddave@gmail.com>
2022-01-12 15:57:48 +0000

    Create 307__inheritance.py

---
+++ 307__inheritance.py
@@ -0,0 +1,207 @@
+## Inheritance
+
+class Animal:
+    def __init__(self, species):
+        self.species = species
+
+
+class Dog(Animal):
+    def __init__(self, name):
+        super().__init__("Dog")
+        self.name = name
+
+    def speak(self):
+        print(f"{self.species} named {self.name}: Woof!")
+
+
+class Cat(Animal):
+    def __init__(self, age):
+        super().__init__("Cat")
+        self.age = age
+
+    def speak(self):
+        print(f"{self.age} year old {self.species}: Miao!")
+
+
+animals = [Dog("Rex"), Cat(13), Dog("Rose")]
+for animal in animals:
+    animal.speak()
+
+"""
+>> Parent / Child <<
+when a class [a] inherits from [b], we say [a] is a child of [b] 
+
+when a class [c] inherits from [d], we say that [d] is a parent of [c]
+
+>> POLYMORPHISM << 
+this is a term originating from biology, where a poly means many and morphism means forms. 
+But in programming it means an object behaving in different ways depending on the contect it is used in 
+
+>> Mehot overriding << 
+when a programmer redefines a method on a class that was already defined in it's parent class
+"""
+
+## very important OOP concept 
+
+"""
+Employee        = a Person
+Manager         = a Person + Employee 
+Owner           = Person 
+
+"""
+
+class Person:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+
+    def say_hello(self): 
+        print(f"Hi my name is {self.firstname} {self.lastname}")
+
+class Employee(Person):         # you can pass multiple classes
+    def __init__(self, firstname, lastname, salary):
+        super().__init__(firstname, lastname)
+        self.salary = salary
+  
+    def say_hello(self):
+        super().say_hello()
+        print(f"My salary is {self.salary}")
+    
+    def test(self):
+        print("test")
+
+e = Person("David", "D")
+e.say_hello()                   # Hi my name is David D
+
+f = Employee("Xiao", "w", 56000)
+f.say_hello()                   # Hi my name is Xiao w
+                                # My salary is 56000
+
+#####################################################
+
+"""
+Person          = a Person
+Employee        = a Person
+Manager         = a Person + Employee 
+Owner           = a Person 
+
+"""
+
+## OTHER Inheritence OR MULTIPLE Ineritence
+
+class Person2:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+
+    def say_hello(self): 
+        print(f"Hi my name is {self.firstname} {self.lastname}")
+
+class Employee2(Person2):         # you can pass multiple classes
+    def __init__(self, firstname, lastname, salary):
+        super().__init__(firstname, lastname)
+        self.salary = salary
+  
+    def say_hello(self):
+        super().say_hello()
+        print(f"My salary is {self.salary}")
+    
+class Manager(Employee2):
+    def __init__(self, firstname, lastname, salary, department):
+        super().__init__(firstname, lastname, salary)
+        self.department = department
+
+class Owner(Person2):
+    def __init__(self, firstname, lastname, net_worth):
+        super().__init__(firstname, lastname)
+        self.net_worth = net_worth
+    
+e = Person2("David 2", "D")
+e.say_hello()                       # Hi my name is David D
+print(type(e))                      # <class '__main__.Person2'>
+
+f = Employee2("Xiao 2", "w", 56000)
+f.say_hello()                       # Hi my name is Xiao w
+                                    # My salary is 56000
+print(type(f))                      # <class '__main__.Employee2'>
+
+m = Manager("Tim", "L", 85000, "bossman")
+m.say_hello()
+print(type(m))                      # <class '__main__.Manager'>
+
+o = Owner("John", "Franklin", 1000000)
+o.say_hello()
+print(type(o))                      # <class '__main__.Owner'>
+print(isinstance(o, Person2))       # True
+
+################################
+
+## multiple inheritence
+
+"""
+this, in python, differs from other programming languages
+
+it can be complex, but is rarely used, however, is interesting when used correctly
+"""
+
+class A:
+    def __init__(self):
+        print("A")
+
+class B:
+    def __init__(self):
+        print("B")
+
+class C(A, B):                      # we go through these classes in order they are defined
+    pass
+
+c = C()                             # output == A
+
+class A2:
+    def __init__(self):
+        print("A")
+
+class B2:
+    def __init__(self):
+        print("B")
+
+class C2(A2, B2):                      # we go through these classes in order they are defined
+    def __init__(self):
+        super().__init__()
+
+c2 = C2()                             # output == A
+
+"""
+MRO = Method Resolution Order
+
+the order in which methods are evaluated against
+
+if not in main class, then look in second super class, then third super class, etc
+"""
+
+###################
+
+## Duck Typing
+
+"""
+if it walks like a duck, flys like a duck, it must be a duck
+
+basically, this is an example of looking at how python will allow a program to compile and only fails when the faulty code is reached
+"""
+
+class Duck: 
+    def swim(self):
+        print("swimming duck")
+
+    def fly(self):
+        print("flying duck")
+
+class Whale: 
+    def swim(self):
+        print("swimming whale")
+
+animals = [Duck(), Duck(), Whale()]
+
+for animal in animals:
+    animal.swim()
+    animal.fly()
