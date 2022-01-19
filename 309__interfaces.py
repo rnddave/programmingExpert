a154161a7034b0b6b064f03436ba85d8a7058c94
David Dickinson <rnddave@gmail.com>
2022-01-19 15:53:50 +0000

    Update 309__interfaces.py

--- 309__interfaces.py
+++ 309__interfaces.py
@@ -85,3 +85,81 @@ check_translator_accuracy("french")
 
 #######################################
 
+"""
+point of an interface is to outline and describe what a class that uses it should look like 
+
+Interfaces are more important in other programming languages, we kind of just fake it in Python 
+
+this is important to know and understand in general, moreso than in Python
+"""
+
+
+class RunCodeInterface:         #Interface is in the name, not mandatory, but good practice to make it stand out
+    def compile_code(self):
+        raise NotImplementedError("You have to implement compile_code")
+
+    def execute_code(self):
+        raise NotImplementedError("you need to implement execute_code")
+
+    
+class GoCode(RunCodeInterface):
+    def comple_code(self):
+        print("Compile Go Code")
+
+    def execute_code(self):
+        print("Execute Go Code")
+
+class JavaCode(RunCodeInterface):
+    def comple_code(self):
+        print("Compile Java Code")
+
+    def execute_code(self):
+        print("Execute Java Code")
+        
+
+def run_code(code : RunCodeInterface):      # note the : and then the link to interface 
+    code.compile_code()
+    code.execute_code()
+
+"""
+python is a weakly typed programming language
+"""
+
+go = GoCode()
+run_code(go)
+
+############# PRACTICE #############
+
+## 309.04 Shapes
+
+import math
+
+
+class ShapeInterface:
+    def get_area(self):
+        raise NotImplementedError
+
+    def get_perimeter(self):
+        raise NotImplementedError
+
+
+class Square(ShapeInterface):
+    def __init__(self, side_length):
+        self.side_length = side_length
+
+    def get_area(self):
+        return self.side_length * self.side_length
+
+    def get_perimeter(self):
+        return self.side_length * 4
+
+
+class Circle(ShapeInterface):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def get_area(self):
+        return math.pi * self.radius ** 2
+
+    def get_perimeter(self):
+        return 2 * math.pi * self.radius
