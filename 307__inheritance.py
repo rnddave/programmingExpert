94dee1292ae2c79f163cb0f313e6afaa9a187636
David Dickinson <rnddave@gmail.com>
2022-01-19 11:44:12 +0000

    308 created
    finished 307 and started 308

--- 307__inheritance.py
+++ 307__inheritance.py
@@ -223,3 +223,51 @@ AttributeError: 'Whale' object has no attribute 'fly'
     """
 
 ############ PRACTICE ###########
+
+## 307.7 - Animal Inheritance
+"""
+create an Animal, Mammal, Reptile class as defined: 
+
+- All Animals have an AGE, WEIGHT, HEIGHT
+- All Mamals have a SEX
+- Most reptiels have 4 LEGS (but not all)
+
+Then create a Monkey, Lizard class that inherrit from proper base class
+
+- MONKEY = 5 FINGERS and COLOR
+- LIZARD = TAIL and COLOR
+"""
+
+class Animal:
+    def __init__(self, age, weight, height):
+        self.age = age
+        self.weight = weight
+        self.height = height
+
+
+class Mammal(Animal):
+    def __init__(self, age, weight, height, sex):
+        super().__init__(age, weight, height)
+        self.sex = sex
+
+
+class Reptile(Animal):
+    def __init__(self, age, weight, height, legs):
+        super().__init__(age, weight, height)
+        self.legs = legs
+
+
+class Monkey(Mammal):
+    fingers = 5
+
+    def __init__(self, age, weight, height, sex, color):
+        super().__init__(age, weight, height, sex)
+        self.color = color
+
+
+class Lizard(Reptile):
+    tail = True
+
+    def __init__(self, age, weight, height, legs, color):
+        super().__init__(age, weight, height, legs)
+        self.color = color
