47b508e98955732b91a5a18d07c701ecc4150b30
David Dickinson <rnddave@gmail.com>
2022-01-20 22:04:31 +0000

    Update 310__operatorOverloading.py

--- 310__operatorOverloading.py
+++ 310__operatorOverloading.py
@@ -335,3 +335,46 @@ print(repr(page1))      # Page(Page ONE, 1)
 
 ###################### TIME FOR PRACTICE ######################
 
+## 310.05 >> VECTOR CLASS
+"""
+write a vector class that implements: 
+
+__init__(self, a, b)
+
+__eq__(self, vector) [returns TRUE if current vector and the vector parameter are equal]
+
+__repr__(self) [return the string representation of the vector]
+
+__add__(self, vector) [create and return a new vector object that has components equal to the sum of the components 
+in the current vector and the vector represented by the vector parameter]
+
+__sub__(self, vector) [create and return a new vector object that has components equal to the difference of the components 
+in the current vector and the vector represented by the vector parameter]
+
+__mul__(self, vector) [returns the dot product of the current vector and the vector represented by the vector parameter
+the dot product of two vectors is equal to the sum of the multiplication of their individual components]
+"""
+
+class Vector:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __eq__(self, vector):
+        return self.a == vector.a and self.b == vector.b
+
+    def __repr__(self):
+        return f"Vector({self.a}, {self.b})"
+
+    def __add__(self, vector):
+        new_a = self.a + vector.a
+        new_b = self.b + vector.b
+        return Vector(new_a, new_b)
+
+    def __sub__(self, vector):
+        new_a = self.a - vector.a
+        new_b = self.b - vector.b
+        return Vector(new_a, new_b)
+
+    def __mul__(self, vector):
+        return self.a * vector.a + self.b * vector.b
