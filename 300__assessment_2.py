0e5082ebdc89d77f4ca085171c0143222208c3c0
David Dickinson <rnddave@gmail.com>
2022-02-04 09:52:23 +0000

    updates

---
+++ 300__assessment_2.py
@@ -0,0 +1,25 @@
+## Student Class
+"""
+defined to keep track of students: 
+
+- instance attribute called [name]
+
+- property called [grade] that returns grade of that student
+-- trying to set a grade should raise [VlaueErro] if the new grade is not between [0] and [100]
+
+- a static method called [calculate_average_grade(students)]
+-- accept list of [Student] objects
+-- return the average grade for those students
+-- if no student in list, then return [-1]
+
+- class variable called [all_students]
+-- stores student objects that have been created in the list 
+
+- class method called [get_average_grade()]
+-- return average grade for all created students
+
+- class method called [get_best_student()]
+-- return student object with the best grade out of all the currently created students
+-- if no students created, the return [None]
+
+"""
