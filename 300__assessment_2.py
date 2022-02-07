d6a67b8020e625f9800f58a30a16161884ac3024
David Dickinson <rnddave@gmail.com>
2022-02-07 13:15:02 +0000

    Update 300__assessment_2.py
    passed the test, but using new items not defined, unsure of best route

--- 300__assessment_2.py
+++ 300__assessment_2.py
@@ -34,19 +34,15 @@ class Student:
         self.grade = grade
         Student.all_students.append(self)
 
-    # - a static method called [calculate_average_grade(students)]
-    @staticmethod
-    def calculate_average_grade(students):
-        # -- if no student in list, then return [-1]
-        if len(students) == 0:
-            return -1
+    @property
+    def grade(self):
+        return self._grade
 
-        # -- return the average grade for those students
-        total = 0
-        for student in students:
-            total += student.grade
-        return total / len(students)
-
+    @grade.setter
+    def grade(self, new_grade):
+        if new_grade < 0 or new_grade > 100:
+            raise ValueError("New grade not in the accepted range of [0-100].")
+        self._grade = new_grade
 
     # - class method called [get_average_grade()]
     @classmethod
@@ -60,7 +56,21 @@ class Student:
         best_student = None
 
         for student in cls.all_students:
-            if best_student == None:
+            if best_student == None or best_student.grade < student.grade:
                 best_student = student
         return best_student
+
+    # - a static method called [calculate_average_grade(students)]
+    @staticmethod
+    def calculate_average_grade(students):
+        # -- if no student in list, then return [-1]
+        if len(students) == 0:
+            return -1
+
+        # -- return the average grade for those students
+        total = 0
+        for student in students:
+            total += student.grade
+        return total / len(students)
+
 
