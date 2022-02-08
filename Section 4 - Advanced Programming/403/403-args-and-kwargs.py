c6e469149b844867100c4a65fe7f77b81ab20257
David Dickinson <rnddave@gmail.com>
2022-02-08 14:58:25 +0000

    402 finished (excluding assessment)

---
+++ Section 4 - Advanced Programming/403/403-args-and-kwargs.py
@@ -0,0 +1,15 @@
+"""
+often times you need to write a function that can accept a number of arguments, you may not always know what thumber will be
+
+*args
+**kwargs
+
+"""
+
+def pretty_print(*args, **kwargs):
+    print("*" * 15)
+    print(*args, **kwargs)
+    print("*" * 15)
+
+
+pretty_print(*["H", "e", "l", "l", "o", "world"], end="!\n")
