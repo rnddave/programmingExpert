3d8c8095baf662802854dbf796512e4c17d48e29
David Dickinson <rnddave@gmail.com>
2022-02-10 16:41:45 +0000

    Create 408-iterators.py

---
+++ Section 4 - Advanced Programming/408/408-iterators.py
@@ -0,0 +1,45 @@
+"""
+Iterators
+
+iteration - again! (this time different)
+
+iterator is a special kind of object that implements a single function [next()]
+
+iterators historically were created through class definitions which had to 
+implement the [__iter__()] method to create the iterator
+in addition to the [__next()] method
+
+However, most modern programmers opt to use the "generator" syntax nowadays due to 
+increased readability and conciseness
+
+"""
+
+# This DividerIterator class is an iterator that will start
+# at a number `start` and divide that number by `factor`
+# over and over again until it is smaller than or equal
+# to `end`.
+
+class DividerIterator:
+    def __init__(self, start, factor, end):
+        self.start = start
+        self.factor = factor
+        self.end = end
+
+    def __iter__(self):
+        self.current = self.start
+        return self
+
+    def __next__(self):
+        if self.current < self.end:
+            raise StopIteration
+        current = self.current
+        self.current = current / self.factor
+        return current
+
+
+# Let's keep dividing 100 by 7 over and over again
+# until 0.01.
+count = 0
+for n in DividerIterator(100, 7, 0.01):
+    print(f"Divided {count} times: {n}")
+    count += 1
