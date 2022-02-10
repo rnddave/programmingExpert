a958ba5aa082275462ce8af9ea3c5f2ecf5ed3d8
David Dickinson <rnddave@gmail.com>
2022-02-10 16:32:01 +0000

    Update 407-decorators.py

--- Section 4 - Advanced Programming/407/407-decorators.py
+++ Section 4 - Advanced Programming/407/407-decorators.py
@@ -110,9 +110,117 @@ get's passed to decorator
 >> to do something before or after the function was run
 """
 
+#######################
+
+# there is a legacy way of doing this as well: 
+
+def foo():
+    print("foo")
+
+foo = decorator(foo)            # this is legacy way of doing this
+
+foo()
+
+# now for some better ways of building decorators
+
+###################################
+# this is why we were introduced to *args and **kwargs first
+###################################
+
+def decorator2(func): 
+    def wrapper(*args, **kwargs):                   # this allows us to take any number of arguents in
+        print("Wrapper function called func!")
+        result = func(*args, **kwargs)
+        return result
+        
+    return wrapper
+
+@decorator2
+def foo(x, y, z=2):
+    print(x, y, z)
+
+foo(2, 3, 4)
+
+"""
+Wrapper function called func!
+2 3 4
+"""
+
+# order matters
+
+"""
+when using mutiple decorators, the order in which you add them matters
+
+"""
+# practice 
+
+###################################
+# 407.03 - add one decorator
+###################################
+"""
+write a decorator function named [add_one] that simply adds ne to the return value of any function it decorates
+
+then use this function to decorate [add_values] function
+
+def add_values(x, y):
+    return x + y
+"""
+
+def add_one(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        result += 1
+        return result
+
+    return wrapper
 
 
+@add_one
+def add_values(x, y):
+    return x + y
 
+# add_values(2, 3)
+# print(add_values)
+
+"""
+my tests were unsuccessful, but I still added it to the code window and it passed all tests, solutions follow
+"""
+# solution 1
+def add_one(func):
+    def wrapper(x, y):
+        return_value = func(x, y)       # strange that this one only takes in 2 parameters,
+                                        # I thought it needed the *args, **kwargs
+        return return_value + 1         # this is the next main difference
+
+    return wrapper
+
+
+@add_one
+def add_values(x, y):
+    return x + y
+
+# solution 2
+def add_one(func):
+    def wrapper(*args, **kwargs):
+        return_value = func(*args, **kwargs)
+        return return_value + 1         # again, main difference, cleaner code, less lines 
+
+    return wrapper
+
+
+@add_one
+def add_values(x, y):
+    return x + y
+
+
+###################################
+# 407.04 - print return value decorator
+###################################
+"""
+write a decorator function named [print_return_value] that prints the return value of any function it decorates
+
+your decorator should work on any function regardless of the number of paremeters
+"""
 
 
 
