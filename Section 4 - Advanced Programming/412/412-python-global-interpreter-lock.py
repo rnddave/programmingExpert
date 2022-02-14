f5d395212f7664dab446170ba1bebea3f1c80a6c
David Dickinson <rnddave@gmail.com>
2022-02-14 14:46:47 +0000

    Create 412-python-global-interpreter-lock.py

---
+++ Section 4 - Advanced Programming/412/412-python-global-interpreter-lock.py
@@ -0,0 +1,15 @@
+"""
+PYTHON GLOBAL INTERPRETER LOCK
+
+- design decision about python architecture
+- unique amongs other programming languages
+
+
+MUTEX
+
+- mutually exclusive lock
+- controlsaccess to a section of code
+- typically used in multi-threaded programs when a section of code should only execute one thread at a time 
+- if a thread has the [mutex] all other threads must wait for it to finish 
+
+"""
