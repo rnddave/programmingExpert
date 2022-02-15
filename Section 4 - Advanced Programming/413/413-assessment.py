a36bd8d9d494293ba65cfe9cde485ca37823e60e
David Dickinson <rnddave@gmail.com>
2022-02-15 16:00:06 +0000

    Update 413-assessment.py
    413.03 starting code

--- Section 4 - Advanced Programming/413/413-assessment.py
+++ Section 4 - Advanced Programming/413/413-assessment.py
@@ -113,6 +113,29 @@ however, when I run it in the programmingExpert.io code playground, it fails wit
 RE-READ the problem - need at least 3 threads to pass, 
 -- but don't know why not having three would generate a timeout error
 
+I modified my code FROM this: 
+
+thread1 = threading.Thread(target=append_values, args=(lst, [1, 3, 5, 3], ))
+thread2 = threading.Thread(target=append_integer, args=(lst, 4))
+
+thread1.start()
+thread2.start()
+thread1.join()
+
+TO THIS: 
+
+thread1 = threading.Thread(target=append_values, args=(lst, [1, 3, 5,], ))
+thread2 = threading.Thread(target=append_integer, args=(lst, 4))
+thread3 = threading.Thread(target=append_values, args=(lst, [3], ))
+
+thread1.start()
+thread2.start()
+thread1.join()
+thread3.start()
+
+OUTPUT is still correct - but still time out in playground, unsure why, 
+I have instead used the example solution to move forward to next question
+
 """
 
 ###################################
@@ -146,3 +169,47 @@ thread2.start()
 thread1.join()
 thread3.start()
 """
+
+###################################
+# 413.03 - Powers of 2 multithread
+###################################
+
+"""
+POWERS OF TWO - MULTI-THREADED
+
+- write program
+- uses multiple threads to gather the ppowers of two included in a range
+
+- program must use at least [2] threads, each responsible for part of the range
+
+- you are given a function called [is_power_of_two] to start with
+-- returns [True] if given number is power of 2
+-- else [False]
+
+"""
+
+# starting code: 
+
+import threading
+
+RANGE_START = 0
+RANGE_END = 1000
+
+"""
+At the end of the program, this variable needs to contain all of the powers
+of 2 within the interval [RANGE_START, RANGE_END).
+"""
+powers_of_two = set()
+
+
+def is_power_of_two(x):
+    if x == 0:
+        return False
+    return (x & (x - 1)) == 0
+
+
+def find_powers_of_two(iter):
+    # Write your code here.
+    pass
+
+
