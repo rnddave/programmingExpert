8af37adb7336703fa6e97de3575ee7d24f1e2f34
David Dickinson <rnddave@gmail.com>
2022-02-15 15:13:52 +0000

    Update 413-assessment.py
    413.01 (before guess answer)

--- Section 4 - Advanced Programming/413/413-assessment.py
+++ Section 4 - Advanced Programming/413/413-assessment.py
@@ -5,3 +5,29 @@ HINTS:
 
 Locks, Delays, .join()
 """
+
+###################################
+# 413.01 - output of following code:
+###################################
+
+import threading
+from time import sleep
+
+def t1():
+    sleep(1)
+    print("thread 1")
+
+def t2():
+    print("thread 2")
+    sleep(1)
+
+def t3():
+    sleep(2)
+    print("thread 3")
+
+thread1 = threading.Thread(target=t1)
+thread2 = threading.Thread(target=t2)
+thread3 = threading.Thread(target=t3)
+thread1.start()
+thread2.start()
+thread3.start()
