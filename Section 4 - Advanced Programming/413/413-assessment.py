ec80098c969a610825d4555e014409d464f60d63
David Dickinson <rnddave@gmail.com>
2022-02-15 15:55:18 +0000

    Update 413-assessment.py
    413.02 complete

--- Section 4 - Advanced Programming/413/413-assessment.py
+++ Section 4 - Advanced Programming/413/413-assessment.py
@@ -70,3 +70,79 @@ def append_integer(lst, integer):
 
 lst = []
 """
+
+import threading
+from time import sleep
+import math
+
+
+def append_values(lst, values=[], delay=1):
+    for value in values:
+        lst.append(value)
+        sleep(math.ceil(abs(delay)))
+        #print(lst)
+
+
+def append_integer(lst, integer):
+    lst.append(integer)
+    #print(lst)
+
+
+lst = []
+#print(lst)
+
+thread1 = threading.Thread(target=append_values, args=(lst, [1, 3, 5,], ))
+thread2 = threading.Thread(target=append_integer, args=(lst, 4))
+thread3 = threading.Thread(target=append_values, args=(lst, [3], ))
+
+thread1.start()
+thread2.start()
+thread1.join()
+thread3.start()
+
+
+
+
+print(lst)          # [1, 4, 3, 5, 3]
+
+"""
+OUTPUT = [1, 4, 3, 5, 3]
+
+however, when I run it in the programmingExpert.io code playground, it fails with a code timeout error
+
+RE-READ the problem - need at least 3 threads to pass, 
+-- but don't know why not having three would generate a timeout error
+
+"""
+
+###################################
+# 413.02 - Starting Threads - SOLUTION
+###################################
+"""
+import threading
+from time import sleep
+import math
+
+
+def append_values(lst, values=[], delay=1):
+    for value in values:
+        lst.append(value)
+        sleep(math.ceil(abs(delay)))
+
+
+def append_integer(lst, integer):
+    lst.append(integer)
+
+
+lst = []
+
+# Write your code here.
+
+thread1 = threading.Thread(target=append_values, args=(lst, [1, 3, 5], 1))
+thread2 = threading.Thread(target=append_integer, args=(lst, 4))
+thread3 = threading.Thread(target=append_integer, args=(lst, 3))
+thread1.start()
+thread2.start()
+thread1.join()
+thread3.start()
+"""
