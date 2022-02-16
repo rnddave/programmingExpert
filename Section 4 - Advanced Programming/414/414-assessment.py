742e458a8158dbf60b4d2062712187e660b47342
David Dickinson <rnddave@gmail.com>
2022-02-16 11:12:40 +0000

    Update 414-assessment.py
    414.05 - complete, no issues

--- Section 4 - Advanced Programming/414/414-assessment.py
+++ Section 4 - Advanced Programming/414/414-assessment.py
@@ -65,5 +65,67 @@ I basically do not understand the question, which does not look well for me
 - such that [lst = [1, 3, 2, 4, 6, 5]] at end of program
 - all functions to be run using [asyncio]
 - code should not  take longer than [1] second to run
+
+STARTING code
+
+import asyncio
+
+
+async def append_two_values(lst, value1, value2):
+    lst.append(value1)
+    await asyncio.sleep(0.5)
+    lst.append(value2)
+
+
+lst = []
 """
 
+import asyncio
+
+
+async def append_two_values(lst, value1, value2):
+    lst.append(value1)
+    await asyncio.sleep(0.5)
+    lst.append(value2)
+
+
+lst = []
+
+async def main():
+    task1 = asyncio.create_task(append_two_values(lst, 1, 4))
+    task2 = asyncio.create_task(append_two_values(lst, 3, 6))
+    task3 = asyncio.create_task(append_two_values(lst, 2, 5))
+
+    await task1
+    await task2 
+    await task3
+
+    print(lst)
+
+asyncio.run(main())
+
+"""
+>>> OUTPUT
+
+[1, 3, 2, 4, 6, 5]
+
+which looks to meet requirement to me
+
+I'm more surprised that this was relatively easy tbh
+"""
+
+###################################
+# 414.05 - Asynchronous Concurrency SOLUTION
+###################################
+
+
+async def main(lst):
+    futures = [append_two_values(lst, 1, 4), append_two_values(lst, 3, 6), append_two_values(lst, 2, 5)]
+    await asyncio.gather(*futures)
+
+
+asyncio.run(main(lst))
+
+"""
+OKAY, that does look tidier and more efficient...
+"""
