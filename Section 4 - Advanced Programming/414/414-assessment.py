c374053c74516e09832250b3165f25807fc8c6df
David Dickinson <rnddave@gmail.com>
2022-02-16 10:50:18 +0000

    Update 414-assessment.py

--- Section 4 - Advanced Programming/414/414-assessment.py
+++ Section 4 - Advanced Programming/414/414-assessment.py
@@ -1,3 +1,20 @@
 ###################################
 # 414.00 - 414 Assessment code
 ###################################
+
+###################################
+# 414.02 - Coroutine = what is?
+###################################
+"""
+any function that can suspend its execution and resume later
+"""
+
+###################################
+# 414.04 - Add One Async
+###################################
+"""
+write function called [add_one]
+- accept a coroutine as mandatory parameter
+- calls the coroutine and returns value +1
+"""
+
