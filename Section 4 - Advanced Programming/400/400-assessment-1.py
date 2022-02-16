ba97739e7ce2e9787adabddaf28a43bb1d5b604e
David Dickinson <rnddave@gmail.com>
2022-02-16 13:24:14 +0000

    Update 400-assessment-1.py
    define the problem

--- Section 4 - Advanced Programming/400/400-assessment-1.py
+++ Section 4 - Advanced Programming/400/400-assessment-1.py
@@ -1,3 +1,15 @@
 ###################################
-# 400.01 - Assessment 1
+# 400.01 - Positive Even Squares
 ###################################
+
+"""
+Write a function that accepts any number of positional arguments, all of which you may assume will be lists of integers
+
+Your function should filter all os these lists such that they only contain even ositive integers 
+- and combine all of the lists into one list of integers
+
+Your function should then modify the combined list such that is contains the squares of all the elements and return the list
+
+Use a cobination of the map, filter, lambda functions/keywords to modify the lists
+
+"""
