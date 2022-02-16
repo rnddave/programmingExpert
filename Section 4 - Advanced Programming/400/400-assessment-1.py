cf0ccd738bda6bc4171dccfc52637caac102a468
David Dickinson <rnddave@gmail.com>
2022-02-16 14:04:06 +0000

    Update 400-assessment-1.py
    with solution added

--- Section 4 - Advanced Programming/400/400-assessment-1.py
+++ Section 4 - Advanced Programming/400/400-assessment-1.py
@@ -37,3 +37,27 @@ try2 = positive_even_squares([2, 5, 7, 2])
 
 print(try1)
 print(try2)
+
+"""
+>> OUTPUT = 
+
+[4, 16]
+[4, 4]
+
+seems to work fine 
+"""
+
+
+###################################
+# 400.01 - Positive Even Squares - SOLUTION
+###################################
+
+def positive_even_squares(*args):
+    positive_even_nums = []
+
+    for lst in args:
+        filtered_list = filter(lambda x: x > 0 and x % 2 == 0, lst)
+        positive_even_nums.extend(filtered_list)
+
+    squares = map(lambda x: x ** 2, positive_even_nums)
+    return list(squares)
