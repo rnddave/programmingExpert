6f616060d77a97775f89b645347e93e67205908f
David Dickinson <rnddave@gmail.com>
2022-02-16 16:09:08 +0000

    Update 400-assessment-3.py
    define the problem - which I don't currently understand!!

--- Section 4 - Advanced Programming/400/400-assessment-3.py
+++ Section 4 - Advanced Programming/400/400-assessment-3.py
@@ -1,3 +1,23 @@
 ###################################
-# 400.03 - Assessment 3
+# 400.03 - GENERATE STRING
 ###################################
+
+"""
+write a generator 
+- accepts a string and an integer called [frequency]
+-- generate sequence as follows
+--- [string[0] * frequency + string[1] * frequency + ... + string[-2] * frequency + string[-1] * frequency]
+
+- your generaor should NOT store this string, it should generate the next element in the sequence each time its [next] method is called 
+
+- you should createthis generator in both a functional and class based was
+
+- functional should be named [generate_string]
+- class should be named [GenerateString]
+
+you may assume that frequency will allywas be >= 0
+
+"""
+
+# WTF - I DON"T even UNDERSTAND the question!!!!!!!!
+
