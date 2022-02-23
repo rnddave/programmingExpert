f509929217dd36cb0d8f06098ce46c856e12967c
David Dickinson <rnddave@gmail.com>
2022-02-23 14:10:17 +0000

    404 complete

--- Section 4 - Advanced Programming/400/400-assessment-4.py
+++ Section 4 - Advanced Programming/400/400-assessment-4.py
@@ -18,3 +18,60 @@ implement following methods:
 -- if this word has not been seen, return 0
 """
 
+import threading
+
+
+class WordCounter:
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.word_counts = {}
+
+    def process_text(self, text):
+        words = text.split(" ")
+        for word in words:
+            self._increment_word_count(word)
+
+    def get_word_count(self, word):
+        self.lock.acquire()
+        count = self.word_counts.get(word, 0)
+        self.lock.release()
+        return count
+
+    def _increment_word_count(self, word):
+        self.lock.acquire()
+        self.word_counts[word] = self.word_counts.get(word, 0) + 1
+        self.lock.release()
+
+
+###################################
+# 400.04 - Thread Safe Counter - SOLUTION
+###################################        
+
+import threading
+
+
+class WordCounter:
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.word_counts = {}
+
+    def process_text(self, text):
+        words = text.split(" ")
+        temporary_word_counts = {}
+        for word in words:
+            if word not in temporary_word_counts:
+                temporary_word_counts[word] = 0
+            temporary_word_counts[word] += 1
+        self._increase_word_counts(temporary_word_counts)
+
+    def get_word_count(self, word):
+        self.lock.acquire()
+        count = self.word_counts.get(word, 0)
+        self.lock.release()
+        return count
+
+    def _increase_word_counts(self, word_counts):
+        self.lock.acquire()
+        for word in word_counts:
+            self.word_counts[word] = self.word_counts.get(word, 0) + word_counts[word]
+        self.lock.release()
