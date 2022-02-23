623a85d29218bda3c27e60b475d8d8d480ac4673
David Dickinson <rnddave@gmail.com>
2022-02-23 14:00:50 +0000

    defining the scope of the 404 exercise

--- Section 4 - Advanced Programming/400/400-assessment-4.py
+++ Section 4 - Advanced Programming/400/400-assessment-4.py
@@ -1,3 +1,20 @@
 ###################################
-# 400.04 - Assessment 4
+# 400.04 - Thread Safe Counter
 ###################################
+
+"""
+create a [WordCounter] class 
+- able to count words in large texts
+- so that a user can quickly calculate how many times a specific word occurs in a string
+
+implement following methods: 
+- [process_text(text)] 
+-- take in a string, [text], and update the internal attributes of WordCounter in a thread-safe manner 
+-- you may assume that [text.split(" ")] is good enough to return the list of words in the passed text
+
+- [get_word_count(word)]
+-- should take n a strin, [text] and check how many times that word has been seen in all the texts that this 
+--- [WordCounter] has processed. 
+-- if this word has not been seen, return 0
+"""
+
