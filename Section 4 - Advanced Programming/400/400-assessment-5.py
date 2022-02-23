2108f89c8e4fa3435300c6ea5c2d78d3c9164ce0
David Dickinson <rnddave@gmail.com>
2022-02-23 14:17:56 +0000

    405 - solution added

--- Section 4 - Advanced Programming/400/400-assessment-5.py
+++ Section 4 - Advanced Programming/400/400-assessment-5.py
@@ -36,3 +36,24 @@ class BatchFetcher:
 working
 """
 
+###################################
+# 400.05 - Asynchronous Fetcher - SOLUTION
+###################################
+
+import asyncio
+
+
+class BatchFetcher:
+    def __init__(self, database):
+        self.database = database
+
+    async def fetch_records(self, record_ids):
+        pending_records = []
+        for record_id in record_ids:
+            task = asyncio.create_task(self.database.async_fetch(record_id))
+            pending_records.append(task)
+
+        records = []
+        for pending_record in pending_records:
+            records.append(await pending_record)
+        return records
