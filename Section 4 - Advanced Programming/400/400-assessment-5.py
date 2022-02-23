3af62b2d01400dcf41bf0afe23fc3c028595259a
David Dickinson <rnddave@gmail.com>
2022-02-23 14:15:46 +0000

    405 - problem defined

--- Section 4 - Advanced Programming/400/400-assessment-5.py
+++ Section 4 - Advanced Programming/400/400-assessment-5.py
@@ -1,3 +1,19 @@
 ###################################
-# 400.05 - Assessment 5
+# 400.05 - Asynchronous Fetcher
 ###################################
+
+"""
+write a [BatchFetcher] class
+- meant to fetch lots of records from a database very quickly
+
+constructor: 
+- takes in a [database] object 
+- has an [async] method called [async_fetch]
+-- this method should take a record identifier [record_id] 
+-- returns whatver the database has in storage for that record 
+
+[BatchFetcher]
+- should implement the async method [fetch_records]
+-- which takes in a list [record_ids]
+-- and should return the list of records corresponding to those in [record_ids]
+"""
